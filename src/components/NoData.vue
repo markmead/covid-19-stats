70e6dafbe9ccf76fd7b7fde5059586aa348d2c6b
markmead <itsmarkmead@gmail.com>
2020-03-16 18:18:56 +0000

    fix no data view

---
+++ src/components/NoData.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-white rounded-lg px-4 pt-5 pb-4 overflow-hidden shadow-xl sm:max-w-sm sm:w-full sm:p-6">
+    <div>
+      <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
+        <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 text-red-600"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+      </div>
+      <div class="mt-3 text-center sm:mt-5">
+        <h3 class="text-lg leading-6 font-medium text-gray-900">
+          Sorry, we have no data!
+        </h3>
+        <div class="mt-2">
+          <p class="text-sm leading-5 text-gray-500">
+            This does not mean there's no cases of COVID-19 (Coranavirus)
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="mt-5 sm:mt-6">
+      <span class="flex w-full rounded-md shadow-sm">
+        <g-link to="/countries" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-indigo-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+          Back to countries
+        </g-link>
+      </span>
+    </div>
+  </div>
+</template>
--- src/templates/Country.vue
+++ src/templates/Country.vue
@@ -1,28 +1,39 @@
 <template>
   <Layout :title="$context.title">
-    <div v-if="!stats">Loading</div>
+    <div v-if="loading">Loading</div>
+    <NoData v-if="error" />
     <ShortStats v-else :stats="stats" :title="$context.title" />
-    {{ this.$context.query }}
   </Layout>
 </template>
 
 <script>
   import axios from 'axios'
   import ShortStats from '~/components/ShortStats'
+  import NoData from '~/components/NoData'
 
   export default {
     data() {
       return {
         stats: '',
+        loading: true,
+        error: false
       }
     },
     components: {
-      ShortStats
+      ShortStats,
+      NoData
     },
     async mounted() {
       await axios
         .get(`https://covid19.mathdro.id/api/countries/${this.$context.query}`)
-        .then(res => this.stats = res.data)
+        .then(res => {
+          this.loading = false
+          this.stats = res.data
+        })
+        .catch(error => {
+          this.loading = false
+          this.error = true
+        })
     }
   }
 </script>
