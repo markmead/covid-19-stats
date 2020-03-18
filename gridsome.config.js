224d14e6462f81f1172e3a60a343112b9aff6fe2
markmead <itsmarkmead@gmail.com>
2020-03-18 21:32:55 +0000

    add sitemap

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,7 +7,10 @@ if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss(require(
 
 module.exports = {
   siteName: 'COVID-19',
-  plugins: [],
+  siteUrl: 'https://covid-19-stats.netlify.com',
+  plugins: [
+    { use: '@gridsome/plugin-sitemap' }
+  ],
   css: {
     loaderOptions: {
       postcss: {
