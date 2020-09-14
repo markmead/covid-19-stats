b3bbaafba616c871b40b1fd5c7f90134a6fa50fe
markmead <itsmarkmead@gmail.com>
2020-09-14 18:36:03 +0100

    add gtag

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,7 +7,17 @@ if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss(require(
 module.exports = {
   siteName: 'COVID-19',
   siteUrl: 'https://covid-19-stats.netlify.com',
-  plugins: [{ use: '@gridsome/plugin-sitemap' }],
+  plugins: [
+    { use: '@gridsome/plugin-sitemap' },
+    {
+      use: 'gridsome-plugin-gtm',
+      options: {
+        id: 'UA-177960351-1',
+        enabled: true,
+        debug: true,
+      },
+    },
+  ],
   css: {
     loaderOptions: {
       postcss: {
